49245950f7660216e622358c0247b77dd86acde3
Abraham Murciano <abrahammurciano@gmail.com>
2022-01-19 22:11:48 +0200

    refactored cog functions

--- modules/role_tag/cog.py
+++ modules/role_tag/cog.py
@@ -13,23 +13,36 @@ class RoleTagsCog(commands.Cog, name="Role Tags"):
 
 	@commands.Cog.listener()
 	async def on_member_update(self, before: discord.Member, after: discord.Member):
-		member = Member(after)
-		if member.current_tags() == member.tags():
+		if before.display_name != after.display_name:
+			func = self.on_member_name_change
+		elif tuple(r.id for r in before.roles) != tuple(r.id for r in after.roles):
+			func = self.on_member_roles_change
+		else:
 			return
-		changed_str = (
-			f'Name "{before.display_name}" changed to "{after.display_name}"'
-			if before.display_name == after.display_name
-			else (
-				f"Roles ({', '.join(r.name for r in before.roles)}) changed to"
-				f" ({', '.join(r.name for r in after.roles)})"
-			)
-		)
+		await func(before, after)
 
-		logger.debug(
-			f'Member "{after.display_name}" changed in guild "{after.guild.name}":'
-			f" {changed_str}"
-		)
-		await member.apply_tags()
+	async def on_member_name_change(
+		self, before: discord.Member, after: discord.Member
+	):
+		member = Member(after)
+		if member.current_tags() != member.tags():
+			logger.debug(
+				f'Member nickname changed in guild "{before.guild.name}":'
+				f' "{before.display_name}" -> "{after.display_name}"'
+			)
+			await member.apply_tags()
+
+	async def on_member_roles_change(
+		self, before: discord.Member, after: discord.Member
+	):
+		member = Member(after)
+		if member.current_tags() != member.tags():
+			logger.debug(
+				f'Member "{after.display_name}" roles changed in guild'
+				f' "{before.guild.name}": {[r.name for r in before.roles]} ->'
+				f" {[r.name for r in after.roles]}"
+			)
+			await member.apply_tags()
 
 	@commands.Cog.listener()
 	async def on_guild_role_update(self, before: discord.Role, after: discord.Role):
